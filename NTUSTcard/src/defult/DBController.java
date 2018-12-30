78babcd5ed7295e5c31f0925cb11bd9ad2e46274
B10515047 <B10515047@mail.ntust.edu.tw>
2018-12-30 22:10:27 +0800

    新增貼文資料庫功能

--- NTUSTcard/src/defult/DBController.java
+++ NTUSTcard/src/defult/DBController.java
@@ -13,7 +13,7 @@ public class DBController {
 			// Class 的靜態 forName() 方法實現動態加載類別
 			Class.forName("com.mysql.jdbc.Driver");
 			// 3306|MySQL開放此端口
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ntustsql?serverTimezone=UTC", "root", "root");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ntustsql?serverTimezone=UTC", "root", "1234");
 			st = con.createStatement();
 
 		} catch (Exception ex) {
@@ -21,7 +21,7 @@ public class DBController {
 		}
 	}
 
-	public ArrayList<UserInfoBean> getData() {
+	public ArrayList<UserInfoBean> getUserData() {
 		ArrayList<UserInfoBean> result = new ArrayList<UserInfoBean>();
 		try {
 			String query = "select * from user";
@@ -38,12 +38,37 @@ public class DBController {
 				data.birthday = rs.getString("birthday");
 				result.add(data);
 			}
+			return result;
 		} catch (Exception ex) {
 			System.out.println(ex);
 		}
 		return result;
 	}
-
-	
-	
-}
+		public ArrayList<PostDataBean> getPostData() {
+			ArrayList<PostDataBean> result = new ArrayList<PostDataBean>();
+			try {
+				String query = "select * from post";
+				rs = st.executeQuery(query);
+				System.out.println("Records for Database");
+				while (rs.next()) {
+					PostDataBean data = new PostDataBean();
+					data.id = rs.getInt("id");
+					data.author = rs.getString("author");
+					data.board = rs.getString("board");
+					data.priority = rs.getInt("priority");
+					data.content = rs.getString("content");
+					data.postTime = rs.getString("postTime");
+					data.heart = rs.getInt("heart");					
+					data.comments = rs.getInt("comments");
+					for(int i=0; i < data.comments;i++)
+					{
+						data.postComment(rs.getString("comment" +Integer.toString(i)));
+					}
+					result.add(data);
+				}
+			} catch (Exception ex) {
+				System.out.println(ex);
+			}	
+			return result;
+		}
+}	
